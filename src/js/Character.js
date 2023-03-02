0adb5b8ab3e8d19756377a30a1e47f2dca733ffd
Vinicius Reno <mineiro.reno@gmail.com>
2023-03-02 14:50:32 -0300

    first commit

---
+++ src/js/Character.js
@@ -0,0 +1,17 @@
+class Character{
+	constructor(charName, village, health, shield, mana ){
+		this.charName = charName;
+		this.village = village;
+		this.health = health;
+		this.shield = shield;
+		this.mana = mana;
+	}
+
+	// Methods
+
+}
+
+const player1 = new Character("vinicius", "leaf", 100, 10, 0);
+const player2 = new Character("vinicius", "cloud", 100, 10, 0);
+const player3 = new Character("vinicius", "rain", 100, 10, 0);
+const player4 = new Character("vinicius", "sand", 100, 10, 0);
